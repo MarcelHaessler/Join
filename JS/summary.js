525f82c297382b7a48bbd71d53ef4af0d025a497
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-11-23 11:22:29 +0100

    add hover effect to pencil btn and done btn

--- JS/summary.js
+++ JS/summary.js
@@ -1,5 +1,7 @@
 
 
+
+//greeting screen
 function getGreetingTextByTime(date = new Date()) {
     const hour = date.getHours(); // local hour 0–23
     if (hour < 12) return "Good morning,";
@@ -12,6 +14,28 @@ function renderGreeting() {
     if (!el) return; // bail out if the element is missing
     el.textContent = getGreetingTextByTime();
 }
+
+//hover imgs 
+function pencilHoverEffect() {
+    const img = document.querySelector('#to-do-container img');
+    if (img) img.src = './assets/img/pencil-button-hover.svg';
+}
+
+function doneHoverEffect() {
+    const img = document.querySelector('#done-container img');
+    if (img) img.src = './assets/img/done-button-hover.svg';
+}
+
+function pencilLeaveEffect() {
+    const img = document.querySelector('#to-do-container img');
+    if (img) img.src = './assets/img/pencil-button.svg';
+}
+
+function doneLeaveEffect() {
+    const img = document.querySelector('#done-container img');
+    if (img) img.src = './assets/img/done-button.svg';
+}
+
 
 // call once on load, or whenever you need to refresh the greeting
 function renderAll(){
