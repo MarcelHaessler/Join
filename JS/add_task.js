cc12b7b3f7b05b8799f7a36e63effb14e962fdac
_Nico_ <kissnicolas16@gmail.com>
2025-12-14 16:11:29 +0100

    new feature - upload created task to Firebase
    Created tasks are now able to be uploaded to firebase. Before the upload it will be checked if all required fields have been filled out.

--- JS/add_task.js
+++ JS/add_task.js
@@ -1,5 +1,6 @@
 let userInitials = '';
-let username = ''
+let username = '';
+
 window.addEventListener("userReady",async (auth) => {
     console.log("Name:",auth.detail.name, "Mail:", auth.detail.email);
     username = auth.detail.name
@@ -22,4 +23,40 @@ function putSelfOnFirstPlace(username) {
    if (array !== -1) contacts.unshift(...contacts.splice(array, 1));
    console.log(contacts);
    
+}
+
+
+function checkFullfilledRequirements() {
+    let taskTitle = document.getElementById('title').value;
+    let taskDescription = document.getElementById('description').value;
+    let taskDueDate = document.getElementById('date').value;
+    let taskPriority = currentPriotity;
+    let taskCategory = currentCategory;
+    let taskAssignments = selectedContacts;
+    let taskSubtasks = subtaskListArray;
+
+    if (taskTitle === '' || taskDescription === '' || taskDueDate === '' || currentCategory === '') {
+        checkTitle();
+        checkDescription();
+        checkDate();
+        checkCategory();
+        return
+    }
+
+    console.log(createTaskObject(taskTitle, taskDescription, taskDueDate, taskPriority, taskCategory, taskAssignments, taskSubtasks));
+    uploadTask(taskTitle, taskDescription, taskDueDate, taskPriority, taskCategory, taskAssignments, taskSubtasks);
+    /*createTaskObject(taskTitle, taskDescription, taskDueDate, taskPriority, taskCategory, taskAssignments, taskSubtasks);*/
+}
+
+function createTaskObject(taskTitle, taskDescription, taskDueDate, taskPriority, taskCategory, taskAssignments, taskSubtasks) {
+    return {
+        title : taskTitle,
+        description : taskDescription,
+        date : taskDueDate,
+        priority: taskPriority,
+        category: taskCategory,
+        assignedPersons: taskAssignments,
+        subtasks: taskSubtasks,
+        createdAt: new Date().toISOString()
+    }
 }
