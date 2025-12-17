1a9964e6cc2f902ec2a8feec90597d29673dee34
Marcel Häßler <marcel.haessler@outlook.com>
2025-12-17 22:06:03 +0100

    fetch.js is work stand alone by Contacts

--- JS/contacts.js
+++ JS/contacts.js
@@ -1,60 +1,3 @@
-const contacts = [
-  {
-    "Name": "Anna Bergmann",
-    "Kürzel": "AB",
-    "Mailadresse": "anna.bergmann@beispiel.de"
-  },
-  {
-    "Name": "Bernd Schmidt",
-    "Kürzel": "BS",
-    "Mailadresse": "bernd.schmidt@beispiel.de"
-  },
-  {
-    "Name": "Bernd Schlauer",
-    "Kürzel": "BS",
-    "Mailadresse": "bernd.schlauer@beispiel.de"
-  },
-  {
-    "Name": "Clara Müller",
-    "Kürzel": "CM",
-    "Mailadresse": "clara.mueller@beispiel.de"
-  },
-  {
-    "Name": "David Wagner",
-    "Kürzel": "DW",
-    "Mailadresse": "david.wagner@beispiel.de"
-  },
-  {
-    "Name": "Elena Koch",
-    "Kürzel": "EK",
-    "Mailadresse": "elena.koch@beispiel.de"
-  },
-  {
-    "Name": "Frank Fischer",
-    "Kürzel": "FF",
-    "Mailadresse": "frank.fischer@beispiel.de"
-  },
-  {
-    "Name": "Gisela Hoffmann",
-    "Kürzel": "GH",
-    "Mailadresse": "gisela.hoffmann@beispiel.de"
-  },
-  {
-    "Name": "Hans Keller",
-    "Kürzel": "HK",
-    "Mailadresse": "hans.keller@beispiel.de"
-  },
-  {
-    "Name": "Ina Richter",
-    "Kürzel": "IR",
-    "Mailadresse": "ina.richter@beispiel.de"
-  },
-  {
-    "Name": "Jens Bauer",
-    "Kürzel": "JB",
-    "Mailadresse": "jens.bauer@beispiel.de"
-  }
-]
 
 window.addEventListener("load", () => {
   nameList();
@@ -65,19 +8,21 @@ const contactDetails = document.getElementById("contactSelectet");
 const contactSidebarCss = document.querySelector('.sideBar');
 const contactDetailsCss = document.querySelector('.contacts');
 
-function nameList() {
-    contacts.sort((a, b) => a.Name.localeCompare(b.Name, "de"));
-    
+async function nameList() {
+    await fetchContacts();
+    console.log("Rendering contact list...");
+    console.log(contacts);
+    contacts.sort((a, b) => a.name.localeCompare(b.name, "de"));
+
     let currentLetter = "";
 
     contacts.forEach(contact => {
-        const letter = contact.Name[0].toUpperCase();
+        const letter = contact.name[0].toUpperCase();
         if (letter !== currentLetter) {
             currentLetter = letter;
             contactList.innerHTML += letterSection(letter);
         }
-        contactList.innerHTML += contactEntry(contact.Name, contact["Kürzel"], contact["Mailadresse"]
-);
+        contactList.innerHTML += contactEntry(contact);
     });
 }
 
@@ -86,7 +31,7 @@ contactList.addEventListener("click", (event) => {
     if (!entry) return;
 
     const mail = entry.dataset.mail;
-    const contact = contacts.find(c => c.Mailadresse === mail);
+    const contact = contacts.find(c => c.email === mail);
 
     contactDetails.innerHTML = showContactDetails(contact);
 
@@ -115,25 +60,26 @@ function letterSection(letter) {
     return `<div class="sectionList">${letter}</div><hr>`
 }
 
-function contactEntry(Name, Kürzel, Mailadresse) {
-    return `<div class="contactEntry"  data-mail="${Mailadresse}">
-                <div class="contactlistIcon">
-                    ${Kürzel}
+function contactEntry(contact) {
+    return `<div class="contactEntry"  data-mail="${contact.email}">
+                <div class="contactlistIcon" style="background-color: ${backgroundColorCodes[contact.colorIndex]};">
+                    ${contact.initials}
                 </div>
                 <div class="contactDitails">
-                    <div class="name">${Name}</div>
-                    <div class="mail">${Mailadresse}</div>
+                    <div class="name">${contact.name}</div>
+                    <div class="mail">${contact.email}</div>
                 </div>
             </div>`
 }
 
 function showContactDetails(contact) {
+    // selectetIcon.style.backgroundColor = backgroundColorCodes[contact.colorIndex];
     return `<div class="selectedIconName">
-                <div class="selectetIcon">
-                    ${contact.Kürzel}
+                <div class="selectetIcon" style="background-color: ${backgroundColorCodes[contact.colorIndex]};">
+                    ${contact.initials}
                 </div>
                 <div class="selectedName">
-                    ${contact.Name}
+                    ${contact.name}
                     <div class="selectedNameButtons">
                         <button><img src="./assets/img/contacts/edit.svg" alt="edit">Edit</button>
                         <button><img src="./assets/img/contacts/delete.svg" alt="delite">Delete</button>
@@ -148,13 +94,13 @@ function showContactDetails(contact) {
                     <th>Email</th>
                 </tr>
                 <tr>
-                    <td class="mail">${contact.Mailadresse}</td>
+                    <td class="mail">${contact.email}</td>
                 </tr>
                 <tr>
                     <th>Phone</th>
                 </tr>
                 <tr>
-                    <td>+49 1234 567890</td>
+                    <td>${contact.phone}</td>
                 </tr>
             </table>`
 }
