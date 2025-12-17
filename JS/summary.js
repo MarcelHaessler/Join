ac2391fa31eb29bdcb6690fd3de20b3434d3bb94
OtschoP <35844438+OtschoP@users.noreply.github.com>
2025-12-17 10:15:50 +0100

    add initials.js

--- JS/summary.js
+++ JS/summary.js
@@ -32,16 +32,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
 window.addEventListener("userReady", (auth) => {
     updateGreeting(auth.detail.name || "");
-    let username = auth.detail.name;
-    userInitials = username.charAt(0).toUpperCase() + username.charAt(username.indexOf(" ")+1).toUpperCase();
-    addInitialToHeader();
 });
 
-function addInitialToHeader() {
-    let initialSpace = document.getElementById('user-initials');
-    initialSpace.innerHTML = userInitials; 
-    console.log(userInitials);
-}
 
 //hover imgs 
 function pencilHoverEffect() {
