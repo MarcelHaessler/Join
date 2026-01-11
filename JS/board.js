9e987efe64c160e4fdda080b87c78217415e4d0a
Marcel Häßler <marcel.haessler@outlook.com>
2026-01-11 17:40:34 +0100

    cleaning Code

--- JS/board.js
+++ JS/board.js
@@ -6,40 +6,24 @@
  const BASE_URL = 'https://join-ad1a9-default-rtdb.europe-west1.firebasedatabase.app/';
 
 
-//  const boards = {
-//   "To do": document.getElementById("todo-tiles"),
-//   "In progress": document.getElementById("progress-tiles"),
-//   "Awaiting feedback": document.getElementById("feedback-tiles"),
-//   "Done": document.getElementById("done-tiles")
-// };
 
 window.addEventListener("tasksLoaded", () => {
-    // fetchTasks();
     updateBoard();
 });
 
 function updateBoard() {
-    // console.log(tasks);
-    // let openToDo = tasks.filter(t => t['taskgroup'] == 'ToDo');
-    // console.log(openToDo.length);
-    // ToDo.innerHTML = '';
-
-    // for (let index = 0; index < openToDo.length; index++) {
-    //     const element = openToDo[index];
-    //     ToDo.innerHTML += generateTodoHTML(element);
-    // }
-
-
     let statuses = ['ToDo', 'InProgress', 'Awaiting', 'Done'];
     statuses.forEach(status => {
         let filteredTasks = tasks.filter(t => t['taskgroup'] == status);
         
         let container = document.getElementById(status);
-        
-        // c) Container leeren
         container.innerHTML = '';
 
-        // d) Elemente rendern (hier nutze ich forEach statt der for-Schleife, ist moderner)
+        if (filteredTasks.length === 0) {
+            container.innerHTML = `<p class="no-tasks-message">No tasks in this category.</p>`;
+            return;
+        }
+
         filteredTasks.forEach(element => {
             container.innerHTML += generateTodoHTML(element);
         });
@@ -73,13 +57,5 @@ async function updateTask(task) {
         },
         body: JSON.stringify({taskgroup: task.taskgroup})
     });
-    generateTodoHTML(task);
+    updateBoard();
 }
-
-// function highlight(id) {
-//     document.getElementById(id).classList.add('drag-area-highlight');
-// }
-
-// function removeHighlight(id) {
-//     document.getElementById(id).classList.remove('drag-area-highlight');
-// }
